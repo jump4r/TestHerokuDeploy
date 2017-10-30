b0254d666c64d4da2987175c7d6ac590eb3372ea
jump4r <jump3r@dggaming.net>
2017-10-30 15:47:12 -0500

    update settings file

--- RepReviews/settings.py
+++ RepReviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_gwe6to007#!j8x*9jm)=n8+sf@d62
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['intense-river-64521.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
