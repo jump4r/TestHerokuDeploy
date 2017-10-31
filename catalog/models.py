b7c99e4ebe1b18bc94df3f313827b32743b2c93a
jump4r <jump3r@dggaming.net>
2017-10-31 14:07:41 -0500

    edit models to allow for more larger text input

--- catalog/models.py
+++ catalog/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     user = models.CharField(max_length=32)
-    date = models.CharField(max_length=30)
-    link = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    link = models.CharField(max_length=150)
     id = models.CharField(max_length=20, primary_key=True)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title + ', ' + self.user
@@ -15,11 +15,11 @@ class Review(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.CharField(max_length=32)
     date = models.IntegerField()
-    itemName = models.CharField(max_length=32)
-    itemLink = models.CharField(max_length=50)
-    itemReview = models.CharField(max_length=250)
+    itemName = models.CharField(max_length=200)
+    itemLink = models.CharField(max_length=150)
+    itemReview = models.CharField(max_length=1500)
     itemSize = models.CharField(max_length=32)
-    itemPic = models.CharField(max_length=75)
+    itemPic = models.CharField(max_length=150)
 
     def __str__(self):
         return self.itemName + ', by ' + self.user
