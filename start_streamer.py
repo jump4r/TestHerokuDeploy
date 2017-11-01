0c48afe9d50d2b89fe57fd1efe40a9d6a9311115
jump4r <jump3r@dggaming.net>
2017-10-31 19:18:22 -0500

    fix streamer settings

--- start_streamer.py
+++ start_streamer.py
@@ -1,5 +1,7 @@
 from catalog.streamer import Streamer
 
 if __name__ == "__main__":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RepReviews.settings')
+    
     s = Streamer()
     s.stream()
