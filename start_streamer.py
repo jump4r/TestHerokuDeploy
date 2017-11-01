ce96da85fdc808388c3dc2e44cee9b7c25e723fd
jump4r <jump3r@dggaming.net>
2017-10-31 19:20:47 -0500

    import os into start_streamer

--- start_streamer.py
+++ start_streamer.py
@@ -1,7 +1,8 @@
 from catalog.streamer import Streamer
+import os 
 
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RepReviews.settings')
-    
+
     s = Streamer()
     s.stream()
